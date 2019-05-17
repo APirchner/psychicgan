019c69c565de20821197475280129a96c4ba841c
AndreasP <andreas.pirchner@student.uibk.ac.at>
2019-05-17 18:06:57 +0200

    added N-dimensional transposed convolution layer

--- model/layers.py
+++ model/layers.py
@@ -3,12 +3,11 @@ import torch.nn as nn
 
 
 class NormConvND(nn.Module):
-    """ Wrapper for N-dimensional convolution with """
+    """ Wrapper for N-dimensional convolution with normalization """
 
     def __init__(self, conv, c_in, c_out, kernel_size, stride, bias=True,
-                 norm=torch.utils.weight_norm, activation_fun=None, padding=0):
+                 norm=nn.utils.weight_norm, activation_fun=None, padding=0):
         """
-
         :param conv: the convolution function, e.g. torch.nn.Conv2D
         :param c_in: the input channels
         :param c_out: the output channels
@@ -31,14 +30,40 @@ class NormConvND(nn.Module):
         return x
 
 
+class NormTransConvND(nn.Module):
+    """ Wrapper for N-dimensional transposed convolution with normalization """
+
+    def __init__(self, trans_conv, c_in, c_out, kernel_size, stride, bias=False,
+                 norm=nn.utils.weight_norm, activation_fun=None, padding=0, output_padding=0):
+        """
+        :param conv: the convolution function, e.g. torch.nn.Conv2D
+        :param c_in: the input channels
+        :param c_out: the output channels
+        :param kernel_size: the kernel size, should be odd
+        :param stride: the stride
+        :param bias: use bias?
+        :param norm: the normalization function for the weights, e.g. torch.utils.weight_norm
+        :param activation_fun: the activation function, e.g. ReLU
+        :param padding: the amount of zero padding
+        :param output_padding: the padding to add to the output
+        """
+        super(NormTransConvND, self).__init__()
+        self.trans_conv = trans_conv(in_channels=c_in, out_channels=c_out, kernel_size=kernel_size,
+                                     stride=stride, bias=bias, padding=padding, output_padding=output_padding)
+        self.norm = norm
+        self.activation_fun = activation_fun
+
+    def forward(self, *input):
+        x = self.norm(self.trans_conv(input)) if self.norm is not None else self.trans_conv(input)
+        x = self.activation_fun(x) if self.activation_fun is not None else x
+        return x
+
+
 class SelfAttentionND(nn.Module):
-    """
-    N-dimensional self attention layer. N is 2 or 3.
-    """
+    """ N-dimensional self attention layer. N is 2 or 3. """
 
     def __init__(self, dim, norm, c_in):
         """
-
         :param dim: input dimension, either 2 or 3
         :param norm: the kind of normalization for the convolutions, e.g. torch.utils.weight_norm
         :param c_in: the input channels
@@ -61,6 +86,11 @@ class SelfAttentionND(nn.Module):
         self.softmax = nn.Softmax(dim=-1)
 
     def forward(self, *input):
+        """
+
+        :param input: layer input
+        :return: tuple of layer output and attention mask
+        """
         # get batch size to infer other dims
         input_size = input.size()
 
@@ -89,4 +119,4 @@ class SelfAttentionND(nn.Module):
         res = self.att_conv(res)
 
         out = input + res
-        return out
+        return out, res
