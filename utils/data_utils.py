3d0dd79da5fbf19fddc73e9fd2c261c53bf24d3d
ge73cey <a.levic@tum.de>
2019-06-24 12:12:25 +0200

    Implemented interface Dataset class for UCF101.

--- utils/data_utils.py
+++ utils/data_utils.py
@@ -7,7 +7,96 @@ import torch
 import torch.utils.data as data
 from PIL import Image, ImageChops, ImageStat
 from torchvision import transforms
+import cv2
 
+class UCF101Data(data.Dataset):
+    
+    def __init__(self, block_in, block_out, shift, skip, folder_path):
+        '''
+        block_in : number of past frames used for prediction
+        block_out : number of frames to predict
+        shift : how much to shift the first frame from a previous first
+        skip: how much to downsample the 25fps
+        folder_path: path to UCF101 folder
+        '''
+        with open(os.path.join(folder_path,'classes.txt'),'r') as file:
+            video_folders = [os.path.join(folder_path,line[:-1]) for line in file]
+        self.all_videos = sorted([os.path.join(x,y) for x in video_folders for y in os.listdir(x)])
+        
+        self.frame_cnt = []
+        for video_path in self.all_videos:
+            vidcap = cv2.VideoCapture(video_path)
+            self.frame_cnt.append(int(vidcap.get(cv2.CAP_PROP_FRAME_COUNT)))
+            
+        self.block_in = block_in
+        self.block_out = block_out
+        self.shift = shift
+        self.skip = skip
+        
+        self.n_blocks = []
+        seglen = block_in + block_out
+        for cnt in self.frame_cnt:
+            self.n_blocks.append((cnt-(seglen-1)*skip-1)//shift+1)
+    
+    def __getitem__(self, key):
+        if isinstance(key, slice):
+            # get the start, stop, and step from the slice
+            return [self[ii] for ii in range(*key.indices(len(self)))]
+        elif isinstance(key, int):
+            # handle negative indices
+            if key < 0:
+                key += len(self)
+            if key < 0 or key >= len(self):
+                raise IndexError("The index (%d) is out of range." % key)
+            # get the data from direct index
+            return self.get_item_from_index(key)
+        else:
+            raise TypeError("Invalid argument type.")
+
+    def __len__(self):
+        return sum(self.n_blocks)
+    
+    def get_item_from_index(self, index):
+        all_blocks = np.cumsum(self.n_blocks)
+        video_idx = np.searchsorted(all_blocks,index)
+        if video_idx==0:
+            block_idx = index
+        else:
+            block_idx = index-all_blocks[video_idx-1]-1
+        frame_idx = [block_idx*self.shift]
+        for k in range(self.block_in + self.block_out - 1):
+            frame_idx.append(frame_idx[-1]+self.skip)
+            
+        print(block_idx,frame_idx)
+        
+        imgs = []
+        targets = []
+        
+        vidcap = cv2.VideoCapture(self.all_videos[video_idx])
+        
+        for k in frame_idx[:self.block_in]:
+            vidcap.set(cv2.CAP_PROP_POS_FRAMES,k)
+            success,image = vidcap.read()
+            # switch R and B (cv2 - BGR, normal - RGB)
+            image = cv2.cvtColor(image,cv2.COLOR_BGR2RGB)
+            imgs.append(image)
+            
+        for k in frame_idx[self.block_in:]:
+            vidcap.set(cv2.CAP_PROP_POS_FRAMES,k)
+            success,image = vidcap.read()
+            # switch R and B (cv2 - BGR, normal - RGB)
+            image = cv2.cvtColor(image,cv2.COLOR_BGR2RGB)
+            targets.append(image)
+
+        imgs = [torch.from_numpy(x).permute(2,0,1) for x in imgs]
+        imgs = torch.stack(imgs, dim=1)
+
+        targets = [torch.from_numpy(x).permute(2,0,1) for x in targets]
+        targets = torch.stack(targets, dim=1)
+        
+        # normalize to (-1, 1)
+
+        return imgs, targets
 
 class KITTIData(data.Dataset):
 
