75adb9f6b77b2dd844ab5c750848e96320adae42
ge73cey <a.levic@tum.de>
2019-06-29 23:39:16 +0200

    Added: configs for WGAN (no bn in disc...), validation for WGAN

--- train.py
+++ train.py
@@ -6,6 +6,7 @@ import torch
 import torch.utils.data as data
 import torch.nn as nn
 import torch.optim as optim
+import torch.autograd as autograd
 import torchvision
 
 from torchsummary import summary
@@ -24,6 +25,24 @@ def weight_init(net):
     elif classname.find('Conv1d') != -1:
         nn.init.xavier_normal_(net.weight.data)
 
+def calc_gradient_penalty(netD, real_data, fake_data, l, device):
+    batch_size = real_data.shape[0]
+    alpha = torch.rand(batch_size, 1)
+    alpha = alpha.to(device)
+
+    interpolates = alpha[:,None,:,None,None] * real_data + ((1 - alpha[:,None,:,None,None]) * fake_data)
+
+    interpolates = autograd.Variable(interpolates, requires_grad=True)
+
+    disc_interpolates, _, _ = netD(interpolates)
+
+    gradients = autograd.grad(outputs=disc_interpolates, inputs=interpolates,
+                              grad_outputs=torch.ones(disc_interpolates.size()).to(device),
+                              create_graph=True, retain_graph=True, only_inputs=True)[0]
+    gradients = gradients.view(gradients.size(0), -1)
+
+    gradient_penalty = ((gradients.norm(2, dim=1) - 1) ** 2).mean() * l
+    return gradient_penalty
 
 if __name__ == '__main__':
 
@@ -42,7 +61,7 @@ if __name__ == '__main__':
     parser.add_argument('-b', '--batch_size', type=int, default=32, help='The batch size')
     parser.add_argument('-g', '--lr_generator', type=float, default=1e-4, help='The generator learning rate')
     parser.add_argument('-', '--lr_encoder', type=float, default=1e-4, help='The encoder learning rate')
-    parser.add_argument('-n', '--lr_discriminator', type=float, default=4e-4, help='The discriminator learning rate')
+    parser.add_argument('-n', '--lr_discriminator', type=float, default=1e-4, help='The discriminator learning rate')
     # CUDA
     parser.add_argument('-c', '--disable-cuda', action='store_true', help='Disable CUDA')
 
@@ -63,10 +82,12 @@ if __name__ == '__main__':
 
     print(device)
 
-    all_data = UCF101Data(args.ins, args.outs, 3, 3, args.dir)
-    lengths = [int(len(all_data) * 0.8), len(all_data) - int(len(all_data) * 0.8)]
-    [train_data, val_data] = data.random_split(all_data, lengths)
-    train_loader = data.DataLoader(train_data, batch_size=args.batch_size, shuffle=True, num_workers=args.workers)
+    all_data = UCF101Data(args.ins, args.outs, 4, 5, args.dir)
+    #lengths = [int(len(all_data) * 0.8), len(all_data) - int(len(all_data) * 0.8)]
+    lengths = [20,20, len(all_data)-40]
+    [train_data, val_data, _] = data.random_split(all_data, lengths)
+    #train_loader = data.DataLoader(train_data, batch_size=args.batch_size, shuffle=True, num_workers=args.workers)
+    train_loader = data.DataLoader(train_data, batch_size=2, shuffle=True, num_workers=args.workers)
     val_loader = data.DataLoader(val_data, batch_size=1, shuffle=False, num_workers=args.workers)
 
     # training objectives
@@ -117,8 +138,48 @@ if __name__ == '__main__':
                                         filters=[16, 32, 64, 128],
                                         attention_at=32, norm=nn.utils.spectral_norm, residual=True)
     else:
-        # TODO: Add configs for Wasserstein
-        raise NotImplementedError
+        if args.config == 1:
+            # basic
+            encoder = Encoder(frame_dim=64, init_temp=args.ins, hidden_dim=args.latent_dim, filters=[16, 32, 64, 128],
+                              attention_at=None, norm=None, residual=True)
+            generator = Generator(frame_dim=64, temporal_target=args.outs, hidden_dim=args.latent_dim,
+                                  filters=[256, 128, 64, 32], attention_at=32, norm=None)
+            discriminator = Discrimator(frame_dim=64, init_temp=args.outs, feature_dim=1, filters=[16, 32, 64, 128],
+                                        attention_at=32, norm=None, batchnorm=False, residual=True)
+        elif args.config == 2:
+            # basic - residual connections
+            encoder = Encoder(frame_dim=64, init_temp=args.ins, hidden_dim=args.latent_dim, filters=[16, 32, 64, 128],
+                              attention_at=None, norm=None, residual=False)
+            generator = Generator(frame_dim=64, temporal_target=args.outs, hidden_dim=args.latent_dim,
+                                  filters=[256, 128, 64, 32], attention_at=32, norm=None)
+            discriminator = Discrimator(frame_dim=64, init_temp=args.outs, feature_dim=1, filters=[16, 32, 64, 128],
+                                        attention_at=32, norm=None, batchnorm=False, residual=False)
+        elif args.config == 3:
+            # basic + spectral norm
+            encoder = Encoder(frame_dim=64, init_temp=args.ins, hidden_dim=args.latent_dim, filters=[16, 32, 64, 128],
+                              attention_at=None, norm=nn.utils.spectral_norm, residual=True)
+            generator = Generator(frame_dim=64, temporal_target=args.outs, hidden_dim=args.latent_dim,
+                                  filters=[256, 128, 64, 32], attention_at=32, norm=nn.utils.spectral_norm)
+            discriminator = Discrimator(frame_dim=64, init_temp=args.outs, feature_dim=1, filters=[16, 32, 64, 128],
+                                        attention_at=32, norm=nn.utils.spectral_norm, batchnorm=False, residual=True)
+        elif args.config == 4:
+            # basic + more filters enc/disc + spectral norm
+            encoder = Encoder(frame_dim=64, init_temp=args.ins, hidden_dim=args.latent_dim, filters=[32, 64, 128, 256],
+                              attention_at=None, norm=nn.utils.spectral_norm, residual=True)
+            generator = Generator(frame_dim=64, temporal_target=args.outs, hidden_dim=args.latent_dim,
+                                  filters=[256, 128, 64, 32], attention_at=32, norm=nn.utils.spectral_norm)
+            discriminator = Discrimator(frame_dim=64, init_temp=args.outs, feature_dim=1,
+                                        filters=[32, 64, 128, 256],
+                                        attention_at=32, norm=nn.utils.spectral_norm, batchnorm=False, residual=True)
+        elif args.config == 5:
+            # basic + different capacities + spectral norm
+            encoder = Encoder(frame_dim=64, init_temp=args.ins, hidden_dim=args.latent_dim, filters=[32, 64, 128, 256],
+                              attention_at=None, norm=nn.utils.spectral_norm, residual=True)
+            generator = Generator(frame_dim=64, temporal_target=args.outs, hidden_dim=args.latent_dim,
+                                  filters=[256, 128, 64, 32], attention_at=32, norm=nn.utils.spectral_norm)
+            discriminator = Discrimator(frame_dim=64, init_temp=args.outs, feature_dim=1,
+                                        filters=[16, 32, 64, 128],
+                                        attention_at=32, norm=nn.utils.spectral_norm, batchnorm=False, residual=True)
 
     encoder = encoder.to(device)
     generator = generator.to(device)
@@ -131,7 +192,7 @@ if __name__ == '__main__':
     # print model summaries
     summary(encoder, input_size=(3, args.ins, 64, 64))
     summary(generator, input_size=(args.latent_dim,))
-    summary(discriminator, input_size=(3, args.ins, 64, 64))
+    summary(discriminator, input_size=(3, args.outs, 64, 64))
 
     # tensorboard log writer
     tb_writer = SummaryWriter(log_dir=args.logdir)
@@ -140,13 +201,17 @@ if __name__ == '__main__':
         # WASSERSTEIN GAN
 
         # in WGAN, moment-based optimizers don't appear to work as well -> see WGAN paper
-        encoder_optim = optim.RMSprop(encoder.parameters(), lr=args.lr_encoder)
-        generator_optim = optim.RMSprop(generator.parameters(), lr=args.lr_generator)
-        discriminator_optim = optim.RMSprop(discriminator.parameters(), lr=args.lr_discriminator)
+        # encoder_optim = optim.RMSprop(encoder.parameters(), lr=args.lr_encoder)
+        # generator_optim = optim.RMSprop(generator.parameters(), lr=args.lr_generator)
+        # discriminator_optim = optim.RMSprop(discriminator.parameters(), lr=args.lr_discriminator)
+        # in improved WGAN they use Adam
+        encoder_optim = optim.Adam(encoder.parameters(), lr=args.lr_encoder, betas=(0.5, 0.9))
+        generator_optim = optim.Adam(generator.parameters(), lr=args.lr_generator, betas=(0.5, 0.9))
+        discriminator_optim = optim.Adam(discriminator.parameters(), lr=args.lr_discriminator, betas=(0.5, 0.9))
 
-        encoder_sched = optim.lr_scheduler.ExponentialLR(encoder_optim, gamma=0.99)
-        generator_sched = optim.lr_scheduler.ExponentialLR(generator_optim, gamma=0.99)
-        discriminator_sched = optim.lr_scheduler.ExponentialLR(discriminator_optim, gamma=0.99)
+        # encoder_sched = optim.lr_scheduler.ExponentialLR(encoder_optim, gamma=0.99)
+        # generator_sched = optim.lr_scheduler.ExponentialLR(generator_optim, gamma=0.99)
+        # discriminator_sched = optim.lr_scheduler.ExponentialLR(discriminator_optim, gamma=0.99)
 
         one = torch.FloatTensor([1]).to(device)
         m_one = -1 * one
@@ -167,21 +232,21 @@ if __name__ == '__main__':
                     out_frames = out_frames.to(device)
 
                     # zero the parameter gradients
-                    encoder_optim.zero_grad()
-                    generator_optim.zero_grad()
+                    encoder.zero_grad()
+                    generator.zero_grad()
                     discriminator.zero_grad()
 
                     # DISCRIMINATOR TRAINING
                     with torch.no_grad():
                         hidden, _ = encoder(in_frames)
                         generated, _ = generator(hidden)
-                    _, out_real, _ = discriminator(out_frames)
-                    _, out_gen, _ = discriminator(generated)
+                    out_real, _, _ = discriminator(out_frames)
+                    out_gen, _, _ = discriminator(generated)
 
                     err_real = out_real.mean(0).view(1)
                     err_gen = out_gen.mean(0).view(1)
 
-                    loss_D = err_gen - err_real
+                    loss_D = err_gen - err_real + calc_gradient_penalty(discriminator, out_frames, generated, 10, device)
                     loss_D.backward()
                     # err_real.backward(m_one)
                     # err_gen.backward(one)
@@ -196,14 +261,14 @@ if __name__ == '__main__':
                 in_frames = in_frames.to(device)
                 out_frames = out_frames.to(device)
 
-                encoder_optim.zero_grad()
-                generator_optim.zero_grad()
+                encoder.zero_grad()
+                generator.zero_grad()
                 discriminator.zero_grad()
 
                 hidden, encoder_attn = encoder(in_frames)
                 generated, generator_attn = generator(hidden)
 
-                _, out_gen, _ = discriminator(generated)
+                out_gen, _, _ = discriminator(generated)
                 err_G = out_gen.mean(0).view(1)
 
                 loss_G = -err_G
@@ -223,8 +288,40 @@ if __name__ == '__main__':
                     gen_imgs = torchvision.utils.make_grid(generated.squeeze())
                     tb_writer.add_image('G_imgs', gen_imgs, global_step=global_step)
 
-                global_step += 1
+                # do the validation
+                if global_step % 100 == 99:
+                    generator.train(False)
+                    encoder.train(False)
+                    discriminator.train(False)
+                    val_loss = 0.0
+                    for in_frames, out_frames in val_loader:
+                        #with torch.no_grad():
+                        encoder.zero_grad()
+                        generator.zero_grad()
+                        discriminator.zero_grad()
+                        # get the validation inputs and outputs
+                        in_frames = in_frames.to(device)
+                        out_frames = out_frames.to(device)
 
+                        # forward
+                        hidden, _ = encoder(in_frames)
+                        generated, _ = generator(hidden)
+                        out_real, _, _ = discriminator(out_frames)
+                        out_gen, _, _ = discriminator(generated)
+
+                        err_real = out_real.mean(0).view(1)
+                        err_gen = out_gen.mean(0).view(1)
+
+                        loss_D = err_gen - err_real + calc_gradient_penalty(discriminator, out_frames, generated, 10, device)
+                        val_loss += loss_D/len(val_data)
+                    print('[Epoch {0} - Step {1}] Val-loss: (D) {2}'.format(
+                        epoch, global_step, round(val_loss.item(), 4)))
+                    tb_writer.add_scalar('val_loss', val_loss.item(), global_step=global_step)
+                    generator.train(True)
+                    encoder.train(True)
+                    discriminator.train(True)
+
+                global_step += 1
 
     else:
         encoder_optim = optim.Adam(encoder.parameters(), lr=args.lr_encoder, betas=(0.0, 0.9))
@@ -250,8 +347,8 @@ if __name__ == '__main__':
                 y_real = torch.zeros((batch_size, 1), dtype=torch.float32).to(device)
 
                 # zero the parameter gradients
-                encoder_optim.zero_grad()
-                generator_optim.zero_grad()
+                encoder.zero_grad()
+                generator.zero_grad()
                 discriminator.zero_grad()
 
                 # DISCRIMINATOR TRAINING
@@ -275,8 +372,8 @@ if __name__ == '__main__':
                 discriminator_optim.step()
 
                 # GENERATOR/ENCODER TRAINING
-                encoder_optim.zero_grad()
-                generator_optim.zero_grad()
+                encoder.zero_grad()
+                generator.zero_grad()
                 discriminator.zero_grad()
 
                 hidden, encoder_attn = encoder(in_frames)
