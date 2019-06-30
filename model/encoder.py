17ad348e03ee069417e4eb45cc633da946f29491
ge49ban <andreas.pirchner@tum.de>
2019-06-30 17:52:32 +0200

    resolved minor bug in encoder temporal downsampling

--- model/encoder.py
+++ model/encoder.py
@@ -30,7 +30,7 @@ class Encoder(nn.Module):
         self.filters = [3]
         self.filters.extend(filters)
 
-        temps = [True if int(np.log2(i)) > int(np.log2(self.target_temp)) else False for i in range(init_temp, 0, -1)]
+        temps = [True if int(np.log2(i)) < int(np.log2(self.target_temp)) else False for i in range(init_temp, 0, -1)]
         temps = temps + [False for i in range(self.depth - len(temps))]
 
         self.linear = layers.NormLinear(c_in=self.target_temp * 4 * 4 * self.filters[-1], c_out=hidden_dim,
